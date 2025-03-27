5d29ac931c25b2f47ee2d0619dd9bb07985f3199
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-27 16:59:51 +0200

    feat: update Roles module to include Sequelize models for Role, User, and UserRoles

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { RolesController } from './roles.controller';
+import { Role } from './roles.model';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from 'src/users/users.model';
+import { UserRoles } from './user-roles.model';
 
 @Module({
   providers: [RolesService],
-  controllers: [RolesController]
+  controllers: [RolesController],
+  imports: [SequelizeModule.forFeature([Role, User, UserRoles])],
 })
 export class RolesModule {}
