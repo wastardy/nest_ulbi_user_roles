7350e49996ffb391415a0ebf7e3afdf7deed9795
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-27 12:49:53 +0200

    feat: implement main entry point for NestJS application

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function start() {
+  const PORT = process.env.PORT || 3000;
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+  });
+}
+
+void start();
