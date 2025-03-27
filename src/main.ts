f34106be830e5e4ec3485facbd87fbd7f0007e8d
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-27 15:49:10 +0200

    feat: integrate Swagger for API documentation setup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function start() {
   const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Practice')
+    .setDescription('REST API Documentation')
+    .setVersion('1.0.0')
+    .addTag('nestjs pet project')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
 
   await app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
