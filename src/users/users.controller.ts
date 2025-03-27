b935221a514f4cd042cd58c36c8f7a0e863aefda
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-27 16:07:57 +0200

    feat: implement user controller with CRUD operations and Swagger documentation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { User } from './users.model';
 
+@ApiTags('Users')
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @ApiOperation({ summary: 'Get all users' })
+  @ApiResponse({ status: 200, type: Array<User> })
+  @Get()
+  getAllUsers() {
+    return this.userService.getAllUsers();
+  }
+
+  @ApiOperation({ summary: 'Get user by id' })
+  @ApiResponse({ status: 200, type: User })
+  @Get(':id')
+  getUserById(id: number) {
+    return this.userService.getUserById(id);
+  }
+
+  @ApiOperation({ summary: 'Create user' })
+  @ApiResponse({ status: 201, type: User })
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.userService.createUser(createUserDto);
+  }
+}
