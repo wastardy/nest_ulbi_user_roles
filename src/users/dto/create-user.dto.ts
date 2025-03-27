deef67a9f1b9e5adc01593bc5f2bc68748ef652a
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-27 16:07:48 +0200

    feat: add API property decorators to CreateUserDto for Swagger documentation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({ example: 'user@gmail.com', description: 'Email' })
   readonly email: string;
+
+  @ApiProperty({ example: '12345', description: 'Password' })
   readonly password: string;
 }
