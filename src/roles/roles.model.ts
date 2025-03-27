56dc2692153644d5c6101727bca046674b32116e
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-27 16:59:37 +0200

    feat: add Role model with properties and relationships for user roles

---
+++ src/roles/roles.model.ts
@@ -0,0 +1,45 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  BelongsToMany,
+  Column,
+  DataType,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+import { User } from 'src/users/users.model';
+import { UserRoles } from './user-roles.model';
+
+interface RoleCreationAttrs {
+  value: string;
+  description: string;
+}
+
+@Table({ tableName: 'roles' })
+export class Role extends Model<Role, RoleCreationAttrs> {
+  @ApiProperty({ example: 1, description: 'Unique identifier' })
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  declare id: number;
+
+  @ApiProperty({ example: 'ADMIN', description: 'Users role' })
+  @Column({
+    type: DataType.STRING,
+    unique: true,
+    allowNull: false,
+  })
+  value: string;
+
+  @ApiProperty({ example: 'Administrator', description: 'Role description' })
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  description: string;
+
+  @BelongsToMany(() => User, () => UserRoles)
+  users: User[];
+}
