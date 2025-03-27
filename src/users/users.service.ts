925bb7a22eda066b07ee011d4c5a29156940fd02
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-27 15:49:39 +0200

    feat: implement user management service with CRUD operations

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { User } from './users.model';
+import { InjectModel } from '@nestjs/sequelize';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User) private userRepository: typeof User) {}
+
+  async getAllUsers(): Promise<User[]> {
+    const users = await this.userRepository.findAll();
+    return users;
+  }
+
+  async getUserById(id: number): Promise<User> {
+    const user = await this.userRepository.findOne({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return user;
+  }
+
+  async createUser(createUserDto: CreateUserDto) {
+    const user = await this.userRepository.create(createUserDto);
+    return user;
+  }
+}
