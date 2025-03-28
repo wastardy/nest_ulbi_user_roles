f6747d50750bfffa05807daca8f0ca8092d6e9ac
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-28 16:09:20 +0200

    feat: add role retrieval and creation endpoints in RolesController with Swagger documentation

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { RolesService } from './roles.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Role } from './roles.model';
+import { CreateRoleDto } from './dto/create-role.dto';
+import { UserRole } from 'src/enums/user-role.enum';
 
+@ApiTags('Roles')
 @Controller('roles')
-export class RolesController {}
+export class RolesController {
+  constructor(private rolesService: RolesService) {}
+
+  @ApiOperation({ summary: 'Get role by value' })
+  @ApiResponse({ status: 200, type: Role })
+  @Get(':value')
+  getRoleByValue(@Param('value') value: UserRole) {
+    return this.rolesService.getRoleByValue(value);
+  }
+
+  @ApiOperation({ summary: 'Create role' })
+  @ApiResponse({ status: 201, type: Role })
+  @Post()
+  createRole(@Body() createRoleDto: CreateRoleDto) {
+    return this.rolesService.createRole(createRoleDto);
+  }
+}
