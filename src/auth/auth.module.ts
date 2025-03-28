203ab1e2f22961986d076d62df0afb95bd9d27f9
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-28 23:19:07 +0200

    feat: integrate JwtModule into AuthModule for JWT token management

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'JWT_SECRET',
+      signOptions: {
+        expiresIn: '1h',
+      },
+    }),
+  ],
 })
 export class AuthModule {}
