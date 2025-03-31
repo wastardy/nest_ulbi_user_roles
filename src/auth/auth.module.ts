df707bb99b3745295b94fc8667b9dd89fcc6c482
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-31 14:01:01 +0300

    feat: add JwtAuthGuard to AuthModule and export it for use in other modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,15 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtAuthGuard } from 'src/middlewares/jwt-auth.guard';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtAuthGuard],
   imports: [
-    UsersModule,
+    forwardRef(() => UsersModule),
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'JWT_SECRET',
       signOptions: {
@@ -16,5 +17,6 @@ import { JwtModule } from '@nestjs/jwt';
       },
     }),
   ],
+  exports: [AuthService, JwtModule, JwtAuthGuard],
 })
 export class AuthModule {}
